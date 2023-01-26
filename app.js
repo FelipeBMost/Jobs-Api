f4843d2c064e427a6383dfbf30f892f1982e020d
Felipe Bolzan Mostardeiro <felipe.bmost@gmail.com>
2023-01-26 17:07:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('express-async-errors');
 // extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
-const css = require('xss-clean')
+const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
 const express = require('express');
